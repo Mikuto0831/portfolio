c05fbbbcac07e553b3d4b532e805f587adfd9666
Mikuto <mikuto1227k@gmail.com>
2024-11-10 11:31:20 +0000

    wip

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -5,24 +5,29 @@ const Index = () => {
     <>
       <div className="mx-[20%]">
         <div className="py-16">
-          <div className="aspect-video bg-liteblue bg-no-repeat bg-cover rounded-3xl \ 
-                          flex items-center">
+          <div
+            className="aspect-video bg-liteblue bg-no-repeat bg-cover rounded-3xl \ 
+                          flex items-center shadow-2xl"
+          >
             <h1 className="pl-[8%] pr-[12%] py-3 text-[3.25vw] title-gradation">
               Mikuto's Portfolio
             </h1>
             <img
               src="./assets/img/mikuto.png"
-              alt="Mikuto"
+              alt="Mikuto by naya @xtrapolis100"
               className="h-full"
-            ></img>
+            />
           </div>
         </div>
 
-        <div className=""></div>
-        <h1 className="text-3xl font-bold underline text-cyan-500">
-          Index Page
-        </h1>
-        <p>ここにホームコンテンツを表示</p>
+        <div className="mx-[14%]">
+          <h1 className="text-[2.5vw] font-bold underline text-cyan-500">
+            About
+          </h1>
+          <div className="mx-4 p-5 bg-cyan-50 rounded-3xl shadow-2xl">
+            
+          </div>
+        </div>
       </div>
     </>
   );
